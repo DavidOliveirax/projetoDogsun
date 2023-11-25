6660272c7d4925b6b232632e61c2dbc0dc44adc8
DavidOliveirax <davidoliveirax92@gmail.com>
2023-11-25 17:31:12 -0300

    feat: add tailwind

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center justify-center text-blue-700 sm:text-red-400 md:text-yellow-500 lg:text-green-950 lg:hidden h-screen text-4xl cursor-pointer">
+
+   <button @click="handleClick">Entre aqui</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+function handleClick(event: MouseEvent){
+  console.log('click')
+}
+
+</script>
+
+<style>
+
+</style>
